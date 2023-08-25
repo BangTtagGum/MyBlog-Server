edc16dc3f988fde3ea201e3afdc62b884080b994
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-08-25 12:44:45 +0900

    Project Settings

---
+++ src/main/java/com/sparta/myblogserver/MyBlogServerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.myblogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogServerApplication.class, args);
+	}
+
+}
