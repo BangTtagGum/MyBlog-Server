9f91007d884fae26c93ff8ce6461efee2c896722
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-09-05 03:09:58 +0900

    :sparkles: Feat: UserController 추가

---
+++ src/main/java/com/sparta/myblogserver/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.sparta.myblogserver.controller;
+
+import com.sparta.myblogserver.dto.user.SignupRequestDto;
+import com.sparta.myblogserver.controller.message.FailureMessage;
+import com.sparta.myblogserver.controller.message.Message;
+import com.sparta.myblogserver.controller.message.SuccessMessage;
+import com.sparta.myblogserver.error.ParameterValidationException;
+import com.sparta.myblogserver.service.UserService;
+import jakarta.validation.Valid;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/login-page")
+    public String loginPage() {
+        return "로그인 페이지 입니다.";
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Message> signup(@RequestBody @Valid SignupRequestDto requestDto,
+            BindingResult bindingResult) {
+//         Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        for (FieldError e : fieldErrors) {
+            throw new ParameterValidationException(e.getDefaultMessage());
+        }
+        userService.signup(requestDto);
+        return ResponseEntity.ok().body(new SuccessMessage("회원 가입 완료"));
+    }
+}
+
