e74493b43299ffa46915b86dd1b103139e7fbbc5
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-09-05 02:45:20 +0900

    :sparkles: Feat: User Entity 추가

---
+++ src/main/java/com/sparta/myblogserver/entity/user/User.java
@@ -0,0 +1,39 @@
+package com.sparta.myblogserver.entity.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+
+@Builder
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = true)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+}
