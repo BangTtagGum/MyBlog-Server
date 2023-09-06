ae6d069f1cf59aaae55413d1775816d654cf2c5e
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-09-06 09:56:14 +0900

    :sparkles: Feat: Login관련 Dto 범위 Validation 추가

--- src/main/java/com/sparta/myblogserver/dto/user/LoginRequestDto.java
+++ src/main/java/com/sparta/myblogserver/dto/user/LoginRequestDto.java
@@ -1,15 +1,17 @@
 package com.sparta.myblogserver.dto.user;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class LoginRequestDto {
 
     @NotBlank
+    @Size(min = 4, max = 10, message = "사용자 이름은 4자 이상 10자 이하입니다.")
     private String username;
 
     @NotBlank
+    @Size(min = 8, max = 15, message = "비밀번호는 8자 이상 15자 이하입니다.")
     private String password;
 }
