4c36558e9aec8ac01b27e41f5867c7bfbad3888d
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-09-06 10:38:24 +0900

    :sparkles: Feat: Login 관련 Dto Validation 응답 메세지 추가

--- src/main/java/com/sparta/myblogserver/dto/user/LoginRequestDto.java
+++ src/main/java/com/sparta/myblogserver/dto/user/LoginRequestDto.java
@@ -7,11 +7,11 @@ import lombok.Getter;
 @Getter
 public class LoginRequestDto {
 
-    @NotBlank
-    @Size(min = 4, max = 10, message = "사용자 이름은 4자 이상 10자 이하입니다.")
+    @NotBlank(message = "사용자 이름을 입력해 주세요.")
+    @Size(min = 4, max = 10, message = "사용자 이름은 4자 이상 10자 이하 입니다.")
     private String username;
 
-    @NotBlank
-    @Size(min = 8, max = 15, message = "비밀번호는 8자 이상 15자 이하입니다.")
+    @NotBlank(message = "비밀번호를 입력해 주세요")
+    @Size(min = 8, max = 15, message = "비밀번호는 8자 이상 15자 이하 입니다.")
     private String password;
 }
