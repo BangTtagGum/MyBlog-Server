5ed5a403798bfa003d89157aa2ac3c86198dec82
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-09-09 01:47:31 +0900

    :sparkles: Feat: Comment 관련 Dto 추가
    CommentRequestDto, CommentResponseDto 추가

---
+++ src/main/java/com/sparta/myblogserver/dto/post/comment/CommentResponseDto.java
@@ -0,0 +1,29 @@
+package com.sparta.myblogserver.dto.post.comment;
+
+import com.sparta.myblogserver.entity.post.comment.Comment;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentResponseDto {
+
+    private Long id;
+    private String content;
+    private String username;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    public CommentResponseDto(Comment comment) {
+        this.id = comment.getId();
+        this.content = comment.getContent();
+        this.username = comment.getUsername();
+        this.createdAt = comment.getCreatedAt();
+        this.modifiedAt = comment.getModifiedAt();
+    }
+}
