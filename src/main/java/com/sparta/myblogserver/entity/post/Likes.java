d06135cdb109d860a4f53b5c6b6c985bef9873ed
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-09-13 15:39:41 +0900

    :sparkles: Feat: 게시글 좋아요 관련 Likes Entity 추가

---
+++ src/main/java/com/sparta/myblogserver/entity/post/Likes.java
@@ -0,0 +1,35 @@
+package com.sparta.myblogserver.entity.post;
+
+import com.sparta.myblogserver.entity.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Likes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Likes(Post post, User user) {
+        this.user = user;
+        this.post = post;
+    }
+}
